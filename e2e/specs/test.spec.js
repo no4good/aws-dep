7f7b0e018e872d08a00a07d5c9ca3379165f36a4
no4good <no4goodll@gmail.com>
2020-07-15 15:11:07 +0300

    docker:e2e fix 0.5

--- e2e/specs/test.spec.js
+++ e2e/specs/test.spec.js
@@ -1,8 +1,10 @@
 const { expect } = require('chai');
+require('dotenv').config();
 
+const { PORT } = process.env;
 describe('My Login application', () => {
   it('should login with valid credentials', async () => {
-    await browser.url('http://localhost:3001');
+    await browser.url(`http://localhost:${PORT}`);
     const body = await $('body');
     const text = await body.getText();
     expect(text).to.eq('GET request to the homepage');
